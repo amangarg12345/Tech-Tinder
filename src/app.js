94fed0af84446fd08e17168586102d569c340834
Aman Garg <amangarg24071995@gmail.com>
2024-10-03 14:21:41 +0530

    User Find by Mail and Get all Users

--- src/app.js
+++ src/app.js
@@ -16,6 +16,39 @@ app.post('/SignUp', async (req, res) => {
     }
 })
 
+app.get('/OneUser', async (req, res) => {
+   const userEmail = req.body.email;
+    try{
+        console.log(userEmail);
+        const user = await userModel.findOne({email : userEmail});
+        if (!user){
+            res.status(400).send("Error to find the User");
+        }
+        else{
+            res.send(user);
+        }
+    }catch(err){
+        res.status(400).send("Error getting the user");
+    }
+})
+
+app.get('/AllUser', async (req, res) => {
+    //const userEmail = req.body.email;
+     try{
+      //   console.log(userEmail);
+         const user = await userModel.find();
+         if (!user){
+             res.status(400).send("Error to find the User");
+         }
+         else{
+             res.send(user);
+         }
+     }catch(err){
+         res.status(400).send("Error getting the user");
+     }
+ })
+
+
 connectDB().then(
     ()=>{
         console.log("Connection Done");     
